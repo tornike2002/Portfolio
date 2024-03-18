ad0cb1510b6e2a38354b276f64a680770e7fa8f2
tornike2002 <toko.toka2000@gmail.com>
2024-03-18 22:32:07 +0400

    added name and username styles in navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { NavLink } from "react-router-dom";
-
+import styled from "styled-components";
 const Navbar: React.FC = () => {
   return (
-    <nav>
+    <Nav>
       <h1>
         Tornike <span>Butiashvili</span>
       </h1>
@@ -11,8 +11,21 @@ const Navbar: React.FC = () => {
         <NavLink to="/">About me</NavLink>
         <NavLink to="/">Projects</NavLink>
       </div>
-    </nav>
+    </Nav>
   );
 };
 
 export default Navbar;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-family: "Roboto", sans-serif;
+  h1 {
+    color: #878787;
+    font-size: 16px;
+    line-height: 2.5;
+    font-weight: 600;
+  }
+`;
