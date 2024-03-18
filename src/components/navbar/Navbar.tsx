e74c00fece461ac0b40860fc3f36c14271aed480
tornike2002 <toko.toka2000@gmail.com>
2024-03-18 23:54:23 +0400

    styled navlinks

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -6,11 +6,11 @@ const Navbar: React.FC = () => {
       <h1>
         Tornike <span>Butiashvili</span>
       </h1>
-      <div>
+      <NavbarLinks>
         <NavLink to="/">Home</NavLink>
-        <NavLink to="/">About me</NavLink>
-        <NavLink to="/">Projects</NavLink>
-      </div>
+        <NavLink to="/about">About me</NavLink>
+        <NavLink to="/projects">Projects</NavLink>
+      </NavbarLinks>
     </Nav>
   );
 };
@@ -18,14 +18,40 @@ const Navbar: React.FC = () => {
 export default Navbar;
 
 const Nav = styled.nav`
+  margin: 0 5%;
   display: flex;
   justify-content: space-between;
   align-items: center;
   font-family: "Roboto", sans-serif;
   h1 {
-    color: #878787;
+    color: #bbb;
     font-size: 16px;
     line-height: 2.5;
     font-weight: 600;
+    span {
+      font-weight: 700;
+      color: #fff;
+      font-size: 16px;
+      line-height: 2.5;
+    }
+  }
+`;
+const NavbarLinks = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+  a {
+    text-decoration: none !important;
+    font-family: "Roboto";
+    font-size: 16px;
+    line-height: 1.5;
+    color: #bbb;
+    transition: all 500ms ease;
+  }
+  a.active {
+    color: #fff;
+  }
+  a:hover {
+    color: #fff;
   }
 `;
