005026f0707d0a45e2e0d99fb4ae60d960619897
tornike2002 <toko.toka2000@gmail.com>
2024-03-19 21:48:56 +0400

    centered title

--- src/components/title/MainTitle.tsx
+++ src/components/title/MainTitle.tsx
@@ -1,21 +1,44 @@
 import { useTypewriter, Cursor } from "react-simple-typewriter";
+import styled from "styled-components";
 const MainTitle: React.FC = () => {
   const [text] = useTypewriter({
-    words: [" I'm React Developer", " I"],
+    words: [
+      " I'm tornike butiashvili",
+      " I'm react developer",
+      " I code cool websites",
+    ],
     loop: true,
-    typeSpeed: 120,
+    typeSpeed: 60,
     deleteSpeed: 50,
   });
   return (
-    <div>
-      <h1>
+    <TitleWrapper>
+      <MainTitleCenter>
         <span>{text}</span>
         <span>
           <Cursor />
         </span>
-      </h1>
-    </div>
+      </MainTitleCenter>
+    </TitleWrapper>
   );
 };
 
 export default MainTitle;
+
+const MainTitleCenter = styled.h1`
+  color: #fff;
+  font-family: "Roboto", sans-serif;
+  span {
+    font-size: 70px;
+    font-weight: 600;
+    line-height: 2.5;
+    letter-spacing: 1px;
+  }
+`;
+const TitleWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 60%;
+
+`;
