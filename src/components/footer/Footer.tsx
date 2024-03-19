c151b7a8d08deb4d4fe344482655ea6cf9c62c2e
tornike2002 <toko.toka2000@gmail.com>
2024-03-19 21:56:59 +0400

    added footer and basic structure

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,23 @@
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div>
+        <div>
+          <h1>E: butiashvilitornike18@gmail.com</h1>
+          <h1>T: +995 598-78-83-00</h1>
+        </div>
+        <div>
+          <p>
+            Hello, I’m Tornike Butiashvili, Front-end Developer
+            Based in Georgia.
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>icons</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
