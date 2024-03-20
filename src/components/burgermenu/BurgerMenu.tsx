15da3ff7f00cc837068a4bd334d9fb40f6a693f9
tornike2002 <toko.toka2000@gmail.com>
2024-03-20 18:53:05 +0400

    added burgermenu structure and bg for good visibility

--- src/components/burgermenu/BurgerMenu.tsx
+++ src/components/burgermenu/BurgerMenu.tsx
@@ -1,9 +1,22 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
 const BurgerMenu: React.FC = () => {
   return (
-    <div>
-      <div>hello</div>
-    </div>
+    <BurgerManuContainer>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/">About</NavLink>
+      <NavLink to="/">Projects</NavLink>
+    </BurgerManuContainer>
   );
 };
 
 export default BurgerMenu;
+const BurgerManuContainer = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6));
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
