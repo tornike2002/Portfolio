480aad3d22bc602bd7743a57dd2567ac4c9bfe2e
tornike2002 <toko.toka2000@gmail.com>
2024-03-26 03:06:11 +0400

    added aboutPage title

--- src/components/Routes/AllRoutes.tsx
+++ src/components/Routes/AllRoutes.tsx
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "../home/Home";
+import AboutPage from "../about/AboutPage";
 
 const AllRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<AboutPage />} />
     </Routes>
   );
 };
--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -1,12 +1,30 @@
-import Navbar from "../navbar/Navbar"
+import Navbar from "../navbar/Navbar";
+import styled from "styled-components";
 
-
-const AboutPage:React.FC = () => {
+const AboutPage: React.FC = () => {
   return (
-    <div>
-        <Navbar />
-    </div>
-  )
-}
+    <AboutMainContainer>
+      <Navbar />
+      <div className="aboutTitle">
+        <h3>Get to know me</h3>
+        <h1>About Me</h1>
+        <hr />
+      </div>
+    </AboutMainContainer>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
+
+const AboutMainContainer = styled.div`
+  background: #101010;
+  min-height: 100vh;
+
+  .aboutTitle {
+    color: #fff;
+
+    h3 {
+      color: #9F9F9F;
+    }
+  }
+`;
