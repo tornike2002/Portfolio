2983eb5b8b7093c8f9aaca2aa3b08fede8cd5dbd
tornike2002 <toko.toka2000@gmail.com>
2024-03-26 03:21:34 +0400

    installed lottie react for animation

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -19,12 +19,28 @@ export default AboutPage;
 const AboutMainContainer = styled.div`
   background: #101010;
   min-height: 100vh;
-
+  font-family: "Poppins", sans-serif;
   .aboutTitle {
     color: #fff;
-
+    text-align: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 15px;
+    margin-top: 40px;
     h3 {
-      color: #9F9F9F;
+      color: #9f9f9f;
+      font-size: 15px;
+      font-weight: 300;
+    }
+    h1 {
+      font-size: 46px;
+      color: #f0f0f0;
+    }
+    hr {
+      border: 3px solid #009e66;
+      width: 75px;
     }
   }
 `;
