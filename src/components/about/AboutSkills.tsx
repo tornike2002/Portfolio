71fdf9acabeed4d8d631a1507a5a371eab095989
tornike2002 <toko.toka2000@gmail.com>
2024-03-31 22:24:08 +0400

    added eperience card

--- src/components/about/AboutSkills.tsx
+++ src/components/about/AboutSkills.tsx
@@ -1,11 +1,13 @@
 import styled from "styled-components";
-const AboutSkills: React.FC = () => {
+import AboutSmallcard from "./AboutSmallcard";
+import { SmallCardProps } from "./AboutSmallcard";
+const AboutSkills: React.FC<SmallCardProps> = () => {
   return (
     <SkillsMainContainer>
-      <div>
+      <div className="SkillsMainWrapper">
         <div className="techstack_card">
-          <h1>Tech stack</h1>
           <ol className="techstack_container">
+            <h1>Tech stack</h1>
             <li>React</li>
             <li>Javascript</li>
             <li>Typescript</li>
@@ -21,12 +23,12 @@ const AboutSkills: React.FC = () => {
         </div>
         {/* work/exp */}
         <div>
-          <h4>2013 - Present</h4>
-          <h1>Art Director - Facebook Inc</h1>
-          <p>
-            Collaborate with creative and development teams on the execution of
-            ideas.
-          </p>
+          <AboutSmallcard
+            company="Legends Hub"
+            title="Experience"
+            present="2024 - present"
+            companyAbout="Collaboration with 2 web developer, UI/UX designer , Backend Developer"
+          />
         </div>
       </div>
     </SkillsMainContainer>
@@ -64,11 +66,17 @@ const SkillsMainContainer = styled.div`
     li:hover {
       color: #3f3;
     }
+    h1 {
+      text-align: center;
+      width: 100%;
+      color: #eee;
+      font-weight: 500 !important;
+      font-size: 21px;
+    }
   }
-  .techstack_card h1 {
-    color: #eee;
-    font-weight: 500;
-    font-size: 21px;
-    text-align: center;
+  .SkillsMainWrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 `;
--- src/components/about/AboutSmallcard.tsx
+++ src/components/about/AboutSmallcard.tsx
@@ -1,9 +1,57 @@
-
-
-const AboutSmallcard: React.FC = () => {
+import styled from "styled-components";
+export type SmallCardProps = {
+  title: string;
+  present: string;
+  company: string;
+  companyAbout: string;
+};
+const AboutSmallcard: React.FC<SmallCardProps> = ({
+  title,
+  present,
+  company,
+  companyAbout,
+}: SmallCardProps) => {
   return (
-    <div>AboutSmallcard</div>
-  )
-}
+    <>
+      <SmallCardContainer>
+        <TitleSmallCard className="smallTitle-center">{title}</TitleSmallCard>
+        <h4>{present}</h4>
+        <h1>{company}</h1>
+        <p>{companyAbout}</p>
+      </SmallCardContainer>
+    </>
+  );
+};
 
-export default AboutSmallcard
+export default AboutSmallcard;
+const SmallCardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 8px;
+  border: 1px solid rgba(255, 255, 255, 0.07);
+  padding: 30px 45px;
+  font-family: "Poppins", sans-serif;
+  h4 {
+    margin: 0 0 5px 0;
+    font-size: 12px;
+    color: #999;
+  }
+  h1,
+  p {
+    color: #eee;
+    font-weight: 500;
+    font-size: 16px;
+  }
+  p {
+    color: #bbb;
+  }
+`;
+const TitleSmallCard = styled.h1`
+  text-align: center;
+  width: 100%;
+  color: #eee;
+  font-weight: 500;
+  font-size: 21px;
+`;
