cc5ac2722696d744df03713560c04b8ee8b65d0c
tornike2002 <toko.toka2000@gmail.com>
2024-03-31 21:40:04 +0400

    added list of techstack

--- src/components/about/AboutSkills.tsx
+++ src/components/about/AboutSkills.tsx
@@ -1,9 +1,31 @@
+import styled from "styled-components";
 const AboutSkills: React.FC = () => {
-  return <div>
-    <div>
-        
-    </div>
-  </div>;
+  return (
+    <SkillsMainContainer>
+      <div>
+        <div>
+          <h1>Tech stack</h1>
+          <ol>
+            <li>React</li>
+            <li>Javascript</li>
+            <li>Typescript</li>
+            <li>Tailwind CSS</li>
+            <li>Styled Components</li>
+            <li>Rest API</li>
+            <li>SCSS</li>
+            <li>Bootstrap</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Git/Github</li>
+          </ol>
+        </div>
+      </div>
+    </SkillsMainContainer>
+  );
 };
 
 export default AboutSkills;
+const SkillsMainContainer = styled.div`
+  min-height: 100vh;
+  max-width: 100%;
+`;
